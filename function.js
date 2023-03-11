e9c33ecf48cea6e9e0979c52cbaf2f3e356b2c29
jamesrufasto <126659959+jamesrufasto@users.noreply.github.com>
2023-03-11 01:20:01 -0300

    initial commit

---
+++ function.js
@@ -0,0 +1,7 @@
+
+document.getElementById('boton').onclick = function(){
+    document.getElementById("demo").innerHTML = "Amo hacer deporte, de preferencia futbol con amigos"; 
+} 
+
+document.getElementById('boton_color').addEventListener('click ', function(){
+    document.body.style.backgroundColor='aqua';})
